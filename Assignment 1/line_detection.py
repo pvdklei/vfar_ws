31b0a13b5805e4c7bb9a47076c062104287c7872
DragonIntelligence <m.i.dragomir@student.vu.nl>
2025-11-08 15:40:28 +0100

    Finished script & recovered field image (accidentally deleted)

--- Assignment 1/line_detection.py
+++ Assignment 1/line_detection.py
@@ -1,23 +1,54 @@
 # WORK IN PROGRESS
 
+import os
 import cv2
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 
-def detect_in_frame(frame, bin_t=250, min_length=50, max_gap=15):
+# Assume you run this in a terminal and are in the Assignment 1 folder of the repo
+# (for automatic default path arguments)
+PATH: str = os.getcwd()
+
+def detect_in_frame(frame, bin_t=250, votes=30, min_length=50, max_gap=15):
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, image = cv2.threshold(image, bin_t, 255, cv2.THRESH_BINARY)
     kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
     image = cv2.dilate(image, kernel, iterations=1)
     image = cv2.Canny(image, 0, 255)
-    lines = cv2.HoughLinesP(image, 1, np.pi/180, 30, minLineLength=min_length, maxLineGap=max_gap)
+    lines = cv2.HoughLinesP(image, 1, np.pi/180, votes, minLineLength=min_length, maxLineGap=max_gap)
     return lines
 
-def display_frame(frame, lines):
-    pass
+def save_frame(filename, frame, lines, dest):
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    cv2.imwrite(f"{dest}/{filename.split(".")[0]}_lines.{filename.split(".")[1]}", frame)
 
-def main():
-    pass
+def main(args):
+    # Get list image path(s)
+    paths: list = []
+    if len(args.src.split(".")) < 2:
+        # This is a folder with a series of frames
+        paths = list(os.walk(args.src))[0][-1]
+    else:
+        # This is a single image
+        paths.append(args.src)
+
+    # Open images & run line detection for each
+    for path in paths:
+        image = cv2.imread(f"{args.src}/{path}")
+        lines = detect_in_frame(image, args.bin_t, args.votes, args.min_length, args.max_gap)
+        save_frame(path, image, lines, args.dest)
 
 if __name__ == "__main__":
-    main()
+    print(PATH)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--src", help="Path to input image or folder", default=PATH + "/images")
+    parser.add_argument("-d", "--dest", help="Path to output folder", default=PATH + "/output")
+    parser.add_argument("-t", "--bin_t", help="Threshold for binarizing the image", default=250)
+    parser.add_argument("-v", "--votes", help="HoughLines parameter of min. nr. votes", default=30)
+    parser.add_argument("--min_length", help="HoughLines parameter of min. line length", default=50)
+    parser.add_argument("--max_gap", help="HoughLines parameter of max. line gap", default=15)
+    args = parser.parse_args()
+    main(args)
