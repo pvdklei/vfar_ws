bd02c106a6dd59fed826d1ce1bd625b75f250bbf
DragonIntelligence <m.i.dragomir@student.vu.nl>
2025-11-08 17:00:39 +0100

    minor fix & new parameters

--- Assignment 1/line_detection.py
+++ Assignment 1/line_detection.py
@@ -10,19 +10,20 @@ import matplotlib.pyplot as plt
 # (for automatic default path arguments)
 PATH: str = os.getcwd()
 
-def detect_in_frame(frame, bin_t=250, votes=30, min_length=50, max_gap=15):
+def detect_in_frame(frame, bin_t=250, votes=70, min_length=50, max_gap=20):
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, image = cv2.threshold(image, bin_t, 255, cv2.THRESH_BINARY)
     kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.dilate(image, kernel, iterations=2)
     image = cv2.Canny(image, 0, 255)
     lines = cv2.HoughLinesP(image, 1, np.pi/180, votes, minLineLength=min_length, maxLineGap=max_gap)
     return lines
 
 def save_frame(filename, frame, lines, dest):
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
     cv2.imwrite(f"{dest}/{filename.split(".")[0]}_lines.{filename.split(".")[1]}", frame)
 
 def main(args):
@@ -46,9 +47,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--src", help="Path to input image or folder", default=PATH + "/images")
     parser.add_argument("-d", "--dest", help="Path to output folder", default=PATH + "/output")
-    parser.add_argument("-t", "--bin_t", help="Threshold for binarizing the image", default=250)
-    parser.add_argument("-v", "--votes", help="HoughLines parameter of min. nr. votes", default=30)
+    parser.add_argument("-t", "--bin_t", help="Threshold for binarizing the image", default=251)
+    parser.add_argument("-v", "--votes", help="HoughLines parameter of min. nr. votes", default=70)
     parser.add_argument("--min_length", help="HoughLines parameter of min. line length", default=50)
-    parser.add_argument("--max_gap", help="HoughLines parameter of max. line gap", default=15)
+    parser.add_argument("--max_gap", help="HoughLines parameter of max. line gap", default=20)
     args = parser.parse_args()
     main(args)
